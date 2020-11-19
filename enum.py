84f84b5a1fa09d454db3fedf6056ff9364a0427a
SolarCoder <74742542+solar-coder@users.noreply.github.com>
2020-11-19 17:32:47 -0500

    Add files via upload

---
+++ enum.py
@@ -0,0 +1,18 @@
+# Author: SolarCoder
+# Instagram | @solarcoder
+
+# initializing a list containing colors of a rainbow
+rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+
+# accessing the (index, element) return value of the enumerate function
+for index, element in enumerate(rainbow):
+
+    print(element + " can be found in position " + str(index) + " of the rainbow.")
+
+
+# Output: Red can be found in position 0 of the rainbow.
+#         Orange can be found in position 1 of the rainbow.
+#         Yellow can be found in position 2 of the rainbow.
+#         Green can be found in position 3 of the rainbow.
+#         Blue can be found in position 4 of the rainbow.
+#         Purple can be found in position 5 of the rainbow.
